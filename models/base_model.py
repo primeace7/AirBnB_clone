ea5f42e289dd34e73e73a949036758ecfb8742c3
primeace7 <osaruonamenjeffery@gmail.com>
2023-07-10 20:05:00 +0000

    Add models, tests directory and task 3 file

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+This module implements the base model class from which every
+other class is created.
+It defines all the fundamental features of all classes in this project.
+'''
+
+class BaseModel():
+    '''
+    Define the base model class
+
+    Args:
+    
